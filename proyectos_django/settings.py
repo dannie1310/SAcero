fee697fd355bce2ad907034e0fc28acf235f7c49
Daniela <dbenitezc@ghi.mx>
2016-07-14 14:27:08 -0500

    se agrego el mensaje nuevo

--- proyectos_django/settings.py
+++ proyectos_django/settings.py
@@ -87,7 +87,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', 
         'NAME': 'control_acero',                 
         'USER': 'root',                    
-        'PASSWORD': '12345',                
+        'PASSWORD': 'root',                
         'HOST': '127.0.0.1',                     
         'PORT': '3306',
         'OPTIONS': {
